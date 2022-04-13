096620d668e24735859eab5d912b329a4c969e4a
Saveliy <svolskey2929@gmail.com>
2022-04-13 14:21:03 +0300

    lesson 3

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,63 +1,61 @@
-// import React from "react";
+import React from "react";
 import Pizza from "./Pizza/Pizza"
 
 import s from './main.module.scss'
+import {useState} from "react";
 
 function Main(props) {
     let pizzas = [
-        {id: 1, name: 'Чизбургер - пицца', image: './img/1.jpg', price: 395},
-        {id: 2, name: 'Сырная', image: './img/2.jpg', price: 450},
-        {id: 3, name: 'Креветки по-азиатски', image: './img/3.jpg', price: 290},
-        {id: 4, name: 'Сырный цыпленок', image: './img/4.jpg', price: 385},
-        {id: 5, name: 'Чизбургер - пицца', image: './img/5.jpg', price: 395},
-        {id: 6, name: 'Сырная', image: './img/6.jpg', price: 395},
-        {id: 7, name: 'Креветки по-азиатски', image: './img/7.jpg', price: 395},
-        {id: 8, name: 'Сырный цыпленок', image: './img/8.jpg', price: 395},
+        {parentId: 1, name: 'Чизбургер - пицца', image: './img/1.jpg', price: 395},
+        {parentId: 2, name: 'Креветки по-азиатски', image: './img/2.jpg', price: 450},
+        {parentId: 3, name: 'Сырная', image: './img/3.jpg', price: 290},
+        {parentId: 4, name: 'Сырный цыпленок', image: './img/4.jpg', price: 385},
+        {parentId: 5, name: 'Чизбургер - пицца', image: './img/5.jpg', price: 395},
+        {parentId: 6, name: 'Креветки по-азиатски', image: './img/6.jpg', price: 395},
+        {parentId: 7, name: 'Сырная', image: './img/7.jpg', price: 395},
+        {parentId: 8, name: 'Сырный цыпленок', image: './img/8.jpg', price: 395},
     ]
+
+    let sortingButton = [{name: 'Все'}, {name: 'Мясные'}, {name: 'Вегетарианская'}, {name: 'Гриль'}, {name: 'Острые'}]
+
+    const [isActiveSort, setIsActiveSort] = useState(0);
+
+    let onSortButton = (index) => {
+        setIsActiveSort(index)
+    }
+
+    const [isSubMenu, setIsSubMenu] = useState(false);
+
     return (
         <div className={s.main}>
             <div className={s.sorting}>
                 <div className={s.sortingLeft}>
-                    <div className={s.sortingButton}>
-                        Все
-                    </div>
-                    <div className={s.sortingButton}>
-                        Мясные
-                    </div>
-                    <div className={s.sortingButton}>
-                        Вегетарианская
-                    </div>
-                    <div className={s.sortingButton}>
-                        Гриль
-                    </div>
-                    <div className={s.sortingButton}>
-                        Острые
-                    </div>
-                    <div className={s.sortingButton}>
-                        Закрытые
-                    </div>
+                    {sortingButton.map((item, index) => (
+                        <div key={`Btn${index}`} className={isActiveSort === index ? s.active : null}
+                             onClick={() => onSortButton(index)}>{item.name}</div>
+                    ))}
                 </div>
                 <div className={s.sortingRight}>
-                    <div className={s.menu}>
+                    <div className={s.menu} onClick={() => setIsSubMenu(!isSubMenu)}>
                         <img src='./img/arrowTop.svg' alt="arrow"/>
                         <p>Сортировка по:</p>
                         <p><span>популярности</span></p>
                     </div>
-                    <div className={s.subMenu}>
+                    {isSubMenu ? <div className={s.subMenu}>
                         <div className={s.features}>
                             <p className={s.item}>популярности</p>
                             <p className={s.item}>по цене</p>
                             <p className={s.item}>по алфавиту</p>
                         </div>
-                    </div>
+                    </div> : null}
                 </div>
             </div>
             <div className={s.pizzasTitle}>Все пиццы</div>
             <div className={s.pizzas}>
                 {pizzas.map((item, index) => (
                     <Pizza
-                        key={index}
-                        id={item.id}
+                        key={`${item.name}${index}`}
+                        parentId={item.parentId}
                         name={item.name}
                         price={item.price}
                         image={item.image}
@@ -67,5 +65,6 @@ function Main(props) {
         </div>
     )
 }
+
 
 export default Main;
