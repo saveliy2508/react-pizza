ec33959091c88117eb219e030ae3ec87d308e4e9
Saveliy <svolskey2929@gmail.com>
2022-04-15 02:13:49 +0300

    lesson 6, redux

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,20 +1,15 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Pizza from "./Pizza/Pizza"
+import axios from "axios";
 
 import s from './main.module.scss'
 import {useState} from "react";
 
 function Main(props) {
-    let pizzas = [
-        {parentId: 1, name: 'Чизбургер - пицца', image: './img/1.jpg', price: 395},
-        {parentId: 2, name: 'Креветки по-азиатски', image: './img/2.jpg', price: 450},
-        {parentId: 3, name: 'Сырная', image: './img/3.jpg', price: 290},
-        {parentId: 4, name: 'Сырный цыпленок', image: './img/4.jpg', price: 385},
-        {parentId: 5, name: 'Чизбургер - пицца', image: './img/5.jpg', price: 395},
-        {parentId: 6, name: 'Креветки по-азиатски', image: './img/6.jpg', price: 395},
-        {parentId: 7, name: 'Сырная', image: './img/7.jpg', price: 395},
-        {parentId: 8, name: 'Сырный цыпленок', image: './img/8.jpg', price: 395},
-    ]
+    const [pizzas, setPizzas] = useState([]);
+    React.useEffect(() => {
+        axios.get('http://localhost:3000/db.json').then((resp) => setPizzas(resp.data.pizzas))
+    }, []);
 
     let sortingButton = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые']
 
@@ -87,7 +82,9 @@ function Main(props) {
                         parentId={item.parentId}
                         name={item.name}
                         price={item.price}
-                        image={item.image}
+                        imageUrl={item.imageUrl}
+                        types={item.types}
+                        sizes={item.sizes}
                     />
                 ))}
             </div>
--- src/components/Main/Pizza/Pizza.js
+++ src/components/Main/Pizza/Pizza.js
@@ -1,26 +1,47 @@
 import React from "react";
+import classNames from "classnames";
 
 import s from './pizza.module.scss'
 
 function Pizza(props) {
+    const typeNames = ['тонкое', 'традиционное'];
+    const size = [26, 30, 40];
+
+    const [activeType, setActiveType] = React.useState(props.types[0]);
+    const onSelectType = (index) => {
+        setActiveType(index)
+    }
+    const [activeSize, setActiveSize] = React.useState(props.sizes[0]==26? 0: props.sizes[0]==30? 1: 2);
+    const onSelectSize = (index) => {
+        setActiveSize(index)
+    }
     return (
         <div className={s.pizza}>
-            <img display='block' className={s.image} src={props.image}></img>
+            <img display='block' className={s.image} src={props.imageUrl}></img>
             <div className={s.title}>{props.name}</div>
             <div className={s.settings}>
                 <div className={s.dough}>
-                    <div>тонкое</div>
-                    <div>традиционное</div>
+                    {typeNames.map((item, index) => (
+                        <div key={'type' + index} className={classNames(
+                            activeType == index ? s.active : null,
+                            !props.types.includes(index) ? s.disabled : null
+                        )}
+                             onClick={() => onSelectType(index)}>{item}</div>
+                    ))}
                 </div>
                 <div className={s.size}>
-                    <div>26 см.</div>
-                    <div>30 см.</div>
-                    <div>40 см.</div>
+                    {size.map((item, index) => (
+                        <div key={'type' + index} className={classNames(
+                            !props.sizes.includes(item) ? s.disabled : null,
+                            activeSize == index ? s.active : null,
+                        )}
+                             onClick={() => onSelectSize(index)}>{item} см.</div>
+                    ))}
                 </div>
             </div>
             <div className={s.footer}>
                 <div className={s.price}>от {props.price} ₽</div>
-                <div className={s.addButton}><img src="./img/plusOrange.svg" alt=""/>Добавить</div>
+                <div className={classNames(s.addButton)}><img src="./img/plusOrange.svg" alt=""/>Добавить</div>
             </div>
         </div>
     )
